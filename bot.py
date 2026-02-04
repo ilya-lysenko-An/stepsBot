75b9eed92ce689bdc4d7280c2722aad2c1203fb7
Ilya Lysenko <ilyalysenko7828@gmail.com>
2026-02-04 13:33:38 +0300

    Логирование и загрузка старта бота

--- bot.py
+++ bot.py
@@ -1,1 +1,27 @@
-print("я готов делать шаги")
+import logging
+import datetime
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+import database
+import os 
+from dotenv import load_dotenv  
+
+load_dotenv("token.env")
+TOKEN = os.getenv("BOT_TOKEN")
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("")
+
+def main():
+    if not TOKEN:
+        logger.error("BOT_TOKEN не найден")
+        return
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+    if __name__ == "__main__":
+        main()
